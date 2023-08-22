e0e9c0d23c5f68e6530e959183ecf092f109708e
Robert Baldyga <robert.baldyga@huawei.com>
2023-08-22 18:18:43 +0300

    bdev/ocf: Introduce IO forward mechanism
    This functionality removes all memory allocations on the IO path by
    creating abstractions which allow to pass all the needed information
    to the bottom volume, using original buffers and IO requests already
    allocated by the user, without the need for creating indirect IO
    representations.
    The responsibility for refcount accounting and error aggregation
    was moved from IO engines to a new intermediate layer introduced by
    forward IO.
    
    Change-Id: Ic77b98196056e675c3dbe65af3977bd2c019d092
    Signed-off-by: Robert Baldyga <robert.baldyga@huawei.com>
    Signed-off-by: Avi Halaf <avi.halaf@huawei.com>
    Signed-off-by: Rafal Stefanowski <rafal.stefanowski@huawei.com>

--- module/bdev/ocf/volume.c
+++ module/bdev/ocf/volume.c
@@ -93,7 +93,7 @@ vbdev_ocf_volume_io_put(struct ocf_io *io)
 }
 
 static int
-get_starting_vec(struct iovec *iovs, int iovcnt, int *offset)
+get_starting_vec(struct iovec *iovs, int iovcnt, uint64_t *offset)
 {
 	int i;
 	size_t off;
@@ -262,8 +262,8 @@ vbdev_ocf_volume_submit_io(struct ocf_io *io)
 		  ocf_volume_get_priv(ocf_io_get_volume(io)));
 	struct ocf_io_ctx *io_ctx = ocf_get_io_ctx(io);
 	struct iovec *iovs;
-	int iovcnt, status = 0, i, offset;
-	uint64_t addr, len;
+	int iovcnt, status = 0, i;
+	uint64_t addr, len, offset;
 
 	if (io->flags == OCF_WRITE_FLUSH) {
 		vbdev_ocf_volume_submit_flush(io);
@@ -382,6 +382,228 @@ vbdev_ocf_volume_get_max_io_size(ocf_volume_t volume)
 	return 131072;
 }
 
+static void
+vbdev_forward_io_cb(struct spdk_bdev_io *bdev_io, bool success, void *opaque)
+{
+	ocf_forward_token_t token = (ocf_forward_token_t) opaque;
+
+	assert(token);
+
+	spdk_bdev_free_io(bdev_io);
+
+	ocf_forward_end(token, success ? 0 : -OCF_ERR_IO);
+}
+
+static void
+vbdev_forward_io_free_iovs_cb(struct spdk_bdev_io *bdev_io, bool success, void *opaque)
+{
+	env_free(bdev_io->u.bdev.iovs);
+	vbdev_forward_io_cb(bdev_io, success, opaque);
+}
+
+static struct spdk_io_channel *
+vbdev_forward_get_channel(ocf_volume_t volume, ocf_forward_token_t token)
+{
+	struct vbdev_ocf_base *base =
+		*((struct vbdev_ocf_base **)
+		  ocf_volume_get_priv(volume));
+	ocf_queue_t queue = ocf_forward_get_io_queue(token);
+	struct vbdev_ocf_qctx *qctx;
+
+	if (unlikely(ocf_queue_is_mngt(queue))) {
+		return base->management_channel;
+	}
+
+	qctx = ocf_queue_get_priv(queue);
+	if (unlikely(qctx == NULL)) {
+		return NULL;
+	}
+
+	return (base->is_cache) ? qctx->cache_ch : qctx->core_ch;
+}
+
+static void
+vbdev_forward_io(ocf_volume_t volume, ocf_forward_token_t token,
+		 int dir, uint64_t addr, uint64_t bytes,
+		 uint64_t offset)
+{
+	struct vbdev_ocf_base *base =
+		*((struct vbdev_ocf_base **)
+		  ocf_volume_get_priv(volume));
+	struct bdev_ocf_data *data = ocf_forward_get_data(token);
+	struct spdk_io_channel *ch;
+	spdk_bdev_io_completion_cb cb = vbdev_forward_io_cb;
+	bool iovs_allocated = false;
+	int iovcnt, skip, status = -1;
+	struct iovec *iovs;
+
+	ch = vbdev_forward_get_channel(volume, token);
+	if (unlikely(ch == NULL)) {
+		ocf_forward_end(token, -EFAULT);
+		return;
+	}
+
+	if (bytes == data->size) {
+		iovs = data->iovs;
+		iovcnt = data->iovcnt;
+	} else {
+		skip = get_starting_vec(data->iovs, data->iovcnt, &offset);
+		if (skip < 0) {
+			SPDK_ERRLOG("Offset bigger than data size\n");
+			ocf_forward_end(token, -OCF_ERR_IO);
+			return;
+		}
+
+		iovcnt = data->iovcnt - skip;
+
+		iovs_allocated = true;
+		cb = vbdev_forward_io_free_iovs_cb;
+		iovs = env_malloc(sizeof(*iovs) * iovcnt, ENV_MEM_NOIO);
+
+		if (!iovs) {
+			SPDK_ERRLOG("Allocation failed\n");
+			ocf_forward_end(token, -OCF_ERR_NO_MEM);
+			return;
+		}
+
+		initialize_cpy_vector(iovs, data->iovcnt, &data->iovs[skip],
+				      iovcnt, offset, bytes);
+	}
+
+	if (dir == OCF_READ) {
+		status = spdk_bdev_readv(base->desc, ch, iovs, iovcnt,
+					 addr, bytes, cb, (void *) token);
+	} else if (dir == OCF_WRITE) {
+		status = spdk_bdev_writev(base->desc, ch, iovs, iovcnt,
+					  addr, bytes, cb, (void *) token);
+	}
+
+	if (unlikely(status)) {
+		SPDK_ERRLOG("Submission failed with status=%d\n", status);
+		/* Since callback is not called, we need to do it manually to free iovs */
+		if (iovs_allocated) {
+			env_free(iovs);
+		}
+		ocf_forward_end(token, (status == -ENOMEM) ? -OCF_ERR_NO_MEM : -OCF_ERR_IO);
+	}
+}
+
+static void
+vbdev_forward_flush(ocf_volume_t volume, ocf_forward_token_t token)
+{
+	struct vbdev_ocf_base *base =
+		*((struct vbdev_ocf_base **)
+		  ocf_volume_get_priv(volume));
+	struct spdk_io_channel *ch;
+	uint64_t bytes = base->bdev->blockcnt * base->bdev->blocklen;
+	int status;
+
+	ch = vbdev_forward_get_channel(volume, token);
+	if (unlikely(ch == NULL)) {
+		ocf_forward_end(token, -EFAULT);
+		return;
+	}
+
+	status = spdk_bdev_flush(
+			 base->desc, ch, 0, bytes,
+			 vbdev_forward_io_cb, (void *)token);
+	if (unlikely(status)) {
+		SPDK_ERRLOG("Submission failed with status=%d\n", status);
+		ocf_forward_end(token, (status == -ENOMEM) ? -OCF_ERR_NO_MEM : -OCF_ERR_IO);
+	}
+}
+
+static void
+vbdev_forward_discard(ocf_volume_t volume, ocf_forward_token_t token,
+		      uint64_t addr, uint64_t bytes)
+{
+	struct vbdev_ocf_base *base =
+		*((struct vbdev_ocf_base **)
+		  ocf_volume_get_priv(volume));
+	struct spdk_io_channel *ch;
+	int status = 0;
+
+	ch = vbdev_forward_get_channel(volume, token);
+	if (unlikely(ch == NULL)) {
+		ocf_forward_end(token, -EFAULT);
+		return;
+	}
+
+	status = spdk_bdev_unmap(
+			 base->desc, ch, addr, bytes,
+			 vbdev_forward_io_cb, (void *)token);
+	if (unlikely(status)) {
+		SPDK_ERRLOG("Submission failed with status=%d\n", status);
+		ocf_forward_end(token, (status == -ENOMEM) ? -OCF_ERR_NO_MEM : -OCF_ERR_IO);
+	}
+}
+
+struct vbdev_forward_io_simple_ctx {
+	ocf_forward_token_t token;
+	struct spdk_io_channel *ch;
+};
+
+static void
+vbdev_forward_io_simple_cb(struct spdk_bdev_io *bdev_io, bool success, void *opaque)
+{
+	struct vbdev_forward_io_simple_ctx *ctx = opaque;
+	ocf_forward_token_t token = ctx->token;
+
+	assert(token);
+
+	spdk_bdev_free_io(bdev_io);
+	spdk_put_io_channel(ctx->ch);
+	env_free(ctx);
+
+	ocf_forward_end(token, success ? 0 : -OCF_ERR_IO);
+}
+
+static void
+vbdev_forward_io_simple(ocf_volume_t volume, ocf_forward_token_t token,
+			int dir, uint64_t addr, uint64_t bytes)
+{
+	struct vbdev_ocf_base *base =
+		*((struct vbdev_ocf_base **)
+		  ocf_volume_get_priv(volume));
+	struct bdev_ocf_data *data = ocf_forward_get_data(token);
+	struct vbdev_forward_io_simple_ctx *ctx;
+	int status = -1;
+
+	ctx = env_malloc(sizeof(*ctx), ENV_MEM_NOIO);
+	if (unlikely(!ctx)) {
+		ocf_forward_end(token, -OCF_ERR_NO_MEM);
+		return;
+	}
+
+	/* Forward IO simple is used in context where queue is not available
+	 * so we have to get io channel ourselves */
+	ctx->ch = spdk_bdev_get_io_channel(base->desc);
+	if (unlikely(ctx->ch == NULL)) {
+		env_free(ctx);
+		ocf_forward_end(token, -EFAULT);
+		return;
+	}
+
+	ctx->token = token;
+
+	if (dir == OCF_READ) {
+		status = spdk_bdev_readv(base->desc, ctx->ch, data->iovs,
+					 data->iovcnt, addr, bytes,
+					 vbdev_forward_io_simple_cb, ctx);
+	} else if (dir == OCF_WRITE) {
+		status = spdk_bdev_writev(base->desc, ctx->ch, data->iovs,
+					  data->iovcnt, addr, bytes,
+					  vbdev_forward_io_simple_cb, ctx);
+	}
+
+	if (unlikely(status)) {
+		SPDK_ERRLOG("Submission failed with status=%d\n", status);
+		spdk_put_io_channel(ctx->ch);
+		env_free(ctx);
+		ocf_forward_end(token, (status == -ENOMEM) ? -OCF_ERR_NO_MEM : -OCF_ERR_IO);
+	}
+}
+
 static struct ocf_volume_properties vbdev_volume_props = {
 	.name = "SPDK_block_device",
 	.io_priv_size = sizeof(struct ocf_io_ctx),
@@ -398,6 +620,10 @@ static struct ocf_volume_properties vbdev_volume_props = {
 		.submit_flush = vbdev_ocf_volume_submit_flush,
 		.get_max_io_size = vbdev_ocf_volume_get_max_io_size,
 		.submit_metadata = vbdev_ocf_volume_submit_metadata,
+		.forward_io = vbdev_forward_io,
+		.forward_flush = vbdev_forward_flush,
+		.forward_discard = vbdev_forward_discard,
+		.forward_io_simple = vbdev_forward_io_simple,
 	},
 	.io_ops = {
 		.set_data = vbdev_ocf_volume_io_set_data,
