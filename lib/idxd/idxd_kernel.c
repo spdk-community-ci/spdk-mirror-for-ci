87a270bed1b7cd0ee4bfc3a5f237ae59cbdc704f
Slawomir Ptak <slawomir.ptak@intel.com>
2023-10-11 11:12:00 +0000

    lib/idxd: Increase max channels per device
    SPDK uses only one work queue to submit work
    to a DSA device. Since the DSA portal address
    space and number of work queue slots are limited,
    there is a limitation for the number of concurrent
    I/O channels which can be used with a single
    DSA device.
    
    This patch increases this limit from 8 to 32,
    since the previous value might have been,
    in some cases, too low.
    
    Change-Id: Ia1d6cf936dc1a3c826009877458a80b8a7edba8d
    Signed-off-by: Slawomir Ptak <slawomir.ptak@intel.com>

--- lib/idxd/idxd_kernel.c
+++ lib/idxd/idxd_kernel.c
@@ -164,7 +164,7 @@ kernel_idxd_probe(void *cb_ctx, spdk_idxd_attach_cb attach_cb, spdk_idxd_probe_c
 
 			/* Since we only use a single WQ, the total size is the size of this WQ */
 			kernel_idxd->idxd.total_wq_size = accfg_wq_get_size(wq);
-			kernel_idxd->idxd.chan_per_device = (kernel_idxd->idxd.total_wq_size >= 128) ? 8 : 4;
+			kernel_idxd->idxd.chan_per_device = (kernel_idxd->idxd.total_wq_size >= 128) ? 32 : 16;
 
 			kernel_idxd->idxd.batch_size = accfg_wq_get_max_batch_size(wq);
 
