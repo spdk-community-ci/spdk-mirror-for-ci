3a72290ba7e2d71ca887225fc0eb8792ca863be2
Jörg Thalheim <joerg@thalheim.io>
2023-10-24 14:30:53 +0200

    python: drop deprecated distutils
    This is scheduled for removal in python 3.12: https://docs.python.org/3/whatsnew/3.12.html
    
    Change-Id: I728dc0cf4ed20f22016d3d58cca8aee3af2bcd8b
    Signed-off-by: Jörg Thalheim <joerg@thalheim.io>

--- python/setup.py
+++ python/setup.py
@@ -2,8 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (C) 2023 Intel Corporation.  All rights reserved.
 
-from distutils.core import setup
-from setuptools import find_packages
+from setuptools import setup, find_packages
 from spdk import __version__
 
 
