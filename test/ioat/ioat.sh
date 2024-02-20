a8fc0c54036623b28722603e249f09d76f29304d
Krzysztof Karas <krzysztof.karas@intel.com>
2024-02-20 09:14:22 +0000

    test/ioat: run setup before testing
    Make sure that there are devices available and attached to userspace
    via setup.sh script before starting the test.
    
    Change-Id: I6f16a086269aad57947903fa41460f6990ac8b93
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>

--- test/ioat/ioat.sh
+++ test/ioat/ioat.sh
@@ -7,6 +7,8 @@ testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
 source $rootdir/test/common/autotest_common.sh
 
+"$rootdir/scripts/setup.sh"
+
 run_test "ioat_perf" $SPDK_EXAMPLE_DIR/ioat_perf -t 1
 
 run_test "ioat_verify" $SPDK_EXAMPLE_DIR/verify -t 1
