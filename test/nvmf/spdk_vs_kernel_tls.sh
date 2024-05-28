45086049516c2fa3fbfeb6427897016fb8377650
Krzysztof Karas <krzysztof.karas@intel.com>
2024-05-28 08:30:15 +0000

    [RFC] test/nvmf: add SPDK target vs kernel initiator test
    Previously it was difficult to test the block device that would be
    added after 'nvme connect' due to stability issues (the device would
    not appear or it would pop-in and then disappear).
    
    After commit 50abcc179e0c ("nvme-tcp: strict pdu pacing to avoid send
    stalls on TLS") has been merged on the kernel side, the stability has
    improved and we now can run IO on the block device.
    
    NOTE: currently this is present on kernel 6.10.0-rc1, so until we
    upgrade to 6.10 in the future, this patch cannot be merged.
    
    Change-Id: I15f8e7aff9b04c6b503491daa44f901a260382e2
    Signed-off-by: Krzysztof Karas <krzysztof.karas@intel.com>

--- test/nvmf/spdk_vs_kernel_tls.sh
+++ test/nvmf/spdk_vs_kernel_tls.sh
@@ -10,6 +10,7 @@ rootdir=$(readlink -f $testdir/../..)
 source $rootdir/test/common/autotest_common.sh
 source $rootdir/test/nvmf/common.sh
 
+fio_py="$rootdir/scripts/fio-wrapper"
 bdevperf_py="$rootdir/examples/bdev/bdevperf/bdevperf.py"
 
 SPEC_KEY="NVMeTLSkey-1:01:VRLbtnN9AQb2WXW3c9+wEf/DRLz0QuLdbYvEhwtdWwNf9LrZ:"
@@ -72,6 +73,23 @@ nvmet_tls_init() {
 	post_configure_kernel_target
 }
 
+setup_nvmf_tgt() {
+	local psk_path=$1
+
+	$rpc_py sock_impl_set_options -i ssl --enable-ktls --tls-version 13
+	$rpc_py framework_start_init
+	$rpc_py nvmf_create_transport $NVMF_TRANSPORT_OPTS
+	$rpc_py nvmf_create_subsystem $SPEC_SUBSYSNQN -s "$NVMF_SERIAL" -m 10
+	$rpc_py nvmf_subsystem_add_listener $SPEC_SUBSYSNQN -t $TEST_TRANSPORT \
+		-a $NVMF_FIRST_TARGET_IP -s $NVMF_PORT -k -c 1
+	$rpc_py bdev_malloc_create 32 4096 -b malloc0
+	$rpc_py nvmf_subsystem_add_ns $SPEC_SUBSYSNQN malloc0 -n 1
+
+	$rpc_py keyring_file_add_key $PSK_NAME "$psk_path"
+
+	$rpc_py nvmf_subsystem_add_host $SPEC_SUBSYSNQN $SPEC_HOSTNQN --psk $PSK_NAME
+}
+
 if [ "$TEST_TRANSPORT" != tcp ]; then
 	echo "Unsupported transport: $TEST_TRANSPORT"
 	exit 1
@@ -103,6 +121,7 @@ timing_exit prepare_keyring_and_daemon
 
 trap "cleanup" SIGINT SIGTERM EXIT
 
+#Test case 1: Kernel target + SPDK initiator (bdevperf)
 nvmet_tls_init
 
 $rootdir/build/examples/bdevperf -m 0x4 -z "${NO_HUGE[@]}" &
@@ -119,6 +138,30 @@ $rpc_py bdev_nvme_get_controllers -n $CONTROLLER_NAME
 $bdevperf_py perform_tests -q 1 -o 4096 -t 5 -w read
 
 $rpc_py bdev_nvme_detach_controller $CONTROLLER_NAME
+killprocess $bdevperfpid
+
+# Test case 2: SPDK target (nvmf_tgt) + Kernel initiator (via nvme-cli)
+timing_enter start_nvmf_tgt
+
+nvmfappstart -m 0x2 --wait-for-rpc
+trap "cleanup" SIGINT SIGTERM EXIT
+waitforlisten $nvmfpid
+setup_nvmf_tgt $SPDK_PSK_PATH
+
+timing_exit start_nvmf_tgt
+
+nvme connect --nqn=$SPEC_SUBSYSNQN --traddr=$NVMF_FIRST_TARGET_IP --trsvcid=$NVMF_PORT \
+	--transport=$TEST_TRANSPORT --hostnqn=$SPEC_HOSTNQN --hostid=$SPEC_HOSTID \
+	--tls -o normal --verbose --tls_key=$key_id --keyring=$keyring_id -i 1
+
+waitforserial $NVMF_SERIAL
+
+$fio_py -p nvmf -i 4096 -d 1 -t read -r 5
+
+nvme disconnect --nqn=$SPEC_SUBSYSNQN &
+
+waitforserial_disconnect $NVMF_SERIAL
+killprocess $nvmfpid
 
 trap - SIGINT SIGTERM EXIT
 cleanup
