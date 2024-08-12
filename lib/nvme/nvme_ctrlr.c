1945329d0df754b3e722dcfe728c2ae96744903c
Ankit Kumar <ankit.kumar@samsung.com>
2024-08-12 07:33:33 +0530

    nvme: interface to retrieve fd for a queue
    Add an interface to retrieve the file descriptor for admin and I/O qpair.
    This is only applicable for the PCIe transport.
    
    Change-Id: I5c24ae15ffc01afea879d0219e8721728cce592e
    Signed-off-by: Ankit Kumar <ankit.kumar@samsung.com>

--- lib/nvme/nvme_ctrlr.c
+++ lib/nvme/nvme_ctrlr.c
@@ -445,6 +445,20 @@ spdk_nvme_ctrlr_disconnect_io_qpair(struct spdk_nvme_qpair *qpair)
 	nvme_ctrlr_unlock(ctrlr);
 }
 
+int
+spdk_nvme_ctrlr_qpair_get_fd(struct spdk_nvme_qpair *qpair)
+{
+	struct spdk_nvme_ctrlr *ctrlr = qpair->ctrlr;
+
+	return nvme_transport_qpair_get_fd(ctrlr, qpair);
+}
+
+int
+spdk_nvme_ctrlr_get_admin_qp_fd(struct spdk_nvme_ctrlr *ctrlr)
+{
+	return spdk_nvme_ctrlr_qpair_get_fd(ctrlr->adminq);
+}
+
 struct spdk_nvme_qpair *
 spdk_nvme_ctrlr_alloc_io_qpair(struct spdk_nvme_ctrlr *ctrlr,
 			       const struct spdk_nvme_io_qpair_opts *user_opts,
