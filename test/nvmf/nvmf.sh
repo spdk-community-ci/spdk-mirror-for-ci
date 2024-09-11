e3d2a3a6fe5cd73113e52536ad022cd9a91ad62a
Michal Berger <michal.berger@intel.com>
2024-09-11 11:19:13 +0200

    test/nvmf: Remove all transport conditions from the test suites
    When given test suite is already executed via `run_test()`, we should
    not return from them without doing any actual work. Instead, steer
    the execution from the very top. Ideally, we should not have similar
    limitations at all as test/nvmf/* in general is not split per
    transport and it may cause overall confusion as to what test is
    executed where (and if at all).
    
    Change-Id: Ic26a862386f07801e36c9659ef272187ea2c1ded
    Signed-off-by: Michal Berger <michal.berger@intel.com>

--- test/nvmf/nvmf.sh
+++ test/nvmf/nvmf.sh
@@ -7,10 +7,6 @@ testdir=$(readlink -f $(dirname $0))
 rootdir=$(readlink -f $testdir/../..)
 source $rootdir/test/common/autotest_common.sh
 
-if [ ! $(uname -s) = Linux ]; then
-	exit 0
-fi
-
 run_test "nvmf_target_core" $rootdir/test/nvmf/nvmf_target_core.sh --transport=$SPDK_TEST_NVMF_TRANSPORT
 run_test "nvmf_target_extra" $rootdir/test/nvmf/nvmf_target_extra.sh --transport=$SPDK_TEST_NVMF_TRANSPORT
 run_test "nvmf_host" $rootdir/test/nvmf/nvmf_host.sh --transport=$SPDK_TEST_NVMF_TRANSPORT
