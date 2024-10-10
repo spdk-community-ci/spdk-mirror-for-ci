da634d5119a5ad0a6b6beca20e279f95b008876d
Michal Berger <michal.berger@intel.com>
2024-10-10 14:08:25 +0200

    test/nvmf: Try to support iWARP under relevant tests
    Mainly targeting e810 NICs.
    
    Change-Id: Icf44beddce7e3abf08745a6b8efb0e5012af9f62
    Signed-off-by: Michal Berger <michal.berger@intel.com>

--- test/nvmf/target/srq_overwhelm.sh
+++ test/nvmf/target/srq_overwhelm.sh
@@ -18,6 +18,7 @@ nvmfappstart -m 0xF
 
 # create the rdma transport with an intentionally small SRQ depth
 $rpc_py nvmf_create_transport $NVMF_TRANSPORT_OPTS -u 8192 -s 1024
+calc_io_queues_for_cqe_irdma 128 4096 6 "$(nproc)"
 
 for i in $(seq 0 5); do
 	$rpc_py nvmf_create_subsystem nqn.2016-06.io.spdk:cnode$i -a -s "SPDK0000000000000${i}"
