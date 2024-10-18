safl/spdk

bdev: Rename _bdev_memory_domain_io_get_buf() by bdev_io_get_bounce_buf()
The following patches will use bounce buffer for DIF insert/strip even if
memory domain is not used.

For clarification, rename _bdev_memory_domain_io_get_buf() by
bdev_io_get_bounce_buf().

Signed-off-by: Shuhei Matsumoto <smatsumoto@nvidia.com>
Change-Id: Ibd20c7385d4bb8a3c376855ae86bde70b5cd4aea

lib/bdev/bdev.c
@@ -3663,8 +3663,8 @@ bdev_io_init_dif_ctx(struct spdk_bdev_io *bdev_io, struct spdk_bdev *bdev)
 }
 
 static void
-_bdev_memory_domain_get_io_cb(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_io,
-			      bool success)
+_bdev_io_get_bounce_buf_cb(struct spdk_io_channel *ch, struct spdk_bdev_io *bdev_io,
+			   bool success)
 {
 	if (!success) {
 		SPDK_ERRLOG("Failed to get data buffer, completing IO\n");
@@ -3688,8 +3688,8 @@ _bdev_memory_domain_get_io_cb(struct spdk_io_channel *ch, struct spdk_bdev_io *b
 }
 
 static void
-_bdev_memory_domain_io_get_buf(struct spdk_bdev_io *bdev_io, spdk_bdev_io_get_buf_cb cb,
-			       uint64_t len)
+_bdev_io_get_bounce_buf(struct spdk_bdev_io *bdev_io, spdk_bdev_io_get_buf_cb cb,
+			uint64_t len)
 {
 	assert(cb != NULL);
 	bdev_io->internal.get_buf_cb = cb;
@@ -3709,8 +3709,8 @@ _bdev_io_ext_use_bounce_buffer(struct spdk_bdev_io *bdev_io)
 	assert(bdev_io->internal.f.has_memory_domain);
 	bdev_io->u.bdev.memory_domain = NULL;
 	bdev_io->u.bdev.memory_domain_ctx = NULL;
-	_bdev_memory_domain_io_get_buf(bdev_io, _bdev_memory_domain_get_io_cb,
-				       bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
+	_bdev_io_get_bounce_buf(bdev_io, _bdev_io_get_bounce_buf_cb,
+				bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
 }
 
 /* We need to allocate bounce buffer if bdev doesn't support memory domains, or if it does

spdk-community-ci/spdk-mirror-for-ci

lib/bdev/bdev.c
@@ -1876,8 +1876,8 @@ spdk_bdev_io_get_buf(struct spdk_bdev_io *bdev_io, spdk_bdev_io_get_buf_cb cb, u
 }
 
 static void
-_bdev_memory_domain_io_get_buf(struct spdk_bdev_io *bdev_io, spdk_bdev_io_get_buf_cb cb,
-			       uint64_t len)
+_bdev_io_get_bounce_buf(struct spdk_bdev_io *bdev_io, spdk_bdev_io_get_buf_cb cb,
+			uint64_t len)
 {
 	assert(cb != NULL);
 	bdev_io->internal.get_buf_cb = cb;
@@ -3710,8 +3710,8 @@ _bdev_io_ext_use_bounce_buffer(struct spdk_bdev_io *bdev_io)
 	assert(bdev_io->internal.f.has_memory_domain);
 	bdev_io->u.bdev.memory_domain = NULL;
 	bdev_io->u.bdev.memory_domain_ctx = NULL;
-	_bdev_memory_domain_io_get_buf(bdev_io, _bdev_memory_domain_get_io_cb,
-				       bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
+	_bdev_io_get_bounce_buf(bdev_io, _bdev_memory_domain_get_io_cb,
+				bdev_io->u.bdev.num_blocks * bdev_io->bdev->blocklen);
 }
 
 /* We need to allocate bounce buffer if bdev doesn't support memory domains, or if it does