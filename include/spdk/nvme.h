safl/spdk

nvme: interface to retrieve fd for a queue
Add an interface to retrieve the file descriptor for admin and I/O qpair.
This is only applicable for the PCIe transport.

Change-Id: I5c24ae15ffc01afea879d0219e8721728cce592e
Signed-off-by: Ankit Kumar <ankit.kumar@samsung.com>

include/spdk/nvme.h
@@ -1906,6 +1906,28 @@ void spdk_nvme_ctrlr_disconnect_io_qpair(struct spdk_nvme_qpair *qpair);
 int spdk_nvme_ctrlr_reconnect_io_qpair(struct spdk_nvme_qpair *qpair);
 
 /**
+ * Get the file descriptor for I/O qpair.
+ *
+ * \param qpair Opaque handle of the qpair for which fd has to be fetched.
+ *
+ * \return a valid event fd on success.
+ * -ENOSYS if transport does not support fetching fd for the qpair.
+ * -EINVAL if fds are not reserved.
+ */
+int spdk_nvme_ctrlr_qpair_get_fd(struct spdk_nvme_qpair *qpair);
+
+/**
+ * Get the file descriptor for admin qpair of the controller.
+ *
+ * \param ctrlr Controller for which fd has to be fetched.
+ *
+ * \return a valid fd on success.
+ * -ENOSYS if transport does not support fetching fd for this controller.
+ * -EINVAL if fd is not reserved.
+ */
+int spdk_nvme_ctrlr_get_adminq_fd(struct spdk_nvme_ctrlr *ctrlr);
+
+/**
  * Returns the reason the admin qpair for a given controller is disconnected.
  *
  * \param ctrlr The controller to check.
@@ -4540,6 +4562,8 @@ struct spdk_nvme_transport_ops {
 	int (*qpair_iterate_requests)(struct spdk_nvme_qpair *qpair,
 				      int (*iter_fn)(struct nvme_request *req, void *arg),
 				      void *arg);
+
+	int (*qpair_get_fd)(struct spdk_nvme_qpair *qpair);
 
 	void (*admin_qpair_abort_aers)(struct spdk_nvme_qpair *qpair);
 

lib/nvme/nvme_ctrlr.c
@@ -445,6 +445,20 @@ spdk_nvme_ctrlr_disconnect_io_qpair(struct spdk_nvme_qpair *qpair)
 	nvme_ctrlr_unlock(ctrlr);
 }
 
+int
+spdk_nvme_ctrlr_qpair_get_fd(struct spdk_nvme_qpair *qpair)
+{
+	struct spdk_nvme_ctrlr *ctrlr = qpair->ctrlr;
+
+	return nvme_transport_qpair_get_fd(ctrlr, qpair);
+}
+
+int
+spdk_nvme_ctrlr_get_adminq_fd(struct spdk_nvme_ctrlr *ctrlr)
+{
+	return spdk_nvme_ctrlr_qpair_get_fd(ctrlr->adminq);
+}
+
 struct spdk_nvme_qpair *
 spdk_nvme_ctrlr_alloc_io_qpair(struct spdk_nvme_ctrlr *ctrlr,
 			       const struct spdk_nvme_io_qpair_opts *user_opts,

spdk-community-ci/spdk-mirror-for-ci

include/spdk/nvme.h
@@ -1916,6 +1916,28 @@ void spdk_nvme_ctrlr_disconnect_io_qpair(struct spdk_nvme_qpair *qpair);
 int spdk_nvme_ctrlr_reconnect_io_qpair(struct spdk_nvme_qpair *qpair);
 
 /**
+ * Get the file descriptor for I/O qpair.
+ *
+ * \param qpair Opaque handle of the qpair for which fd has to be fetched.
+ *
+ * \return a valid event fd on success.
+ * -ENOSYS if transport does not support fetching fd for the qpair.
+ * -EINVAL if fds are not reserved.
+ */
+int spdk_nvme_ctrlr_qpair_get_fd(struct spdk_nvme_qpair *qpair);
+
+/**
+ * Get the file descriptor for admin qpair of the controller.
+ *
+ * \param ctrlr Controller for which fd has to be fetched.
+ *
+ * \return a valid fd on success.
+ * -ENOSYS if transport does not support fetching fd for this controller.
+ * -EINVAL if fd is not reserved.
+ */
+int spdk_nvme_ctrlr_get_admin_qp_fd(struct spdk_nvme_ctrlr *ctrlr);
+
+/**
  * Returns the reason the admin qpair for a given controller is disconnected.
  *
  * \param ctrlr The controller to check.
@@ -4550,6 +4572,8 @@ struct spdk_nvme_transport_ops {
 	int (*qpair_iterate_requests)(struct spdk_nvme_qpair *qpair,
 				      int (*iter_fn)(struct nvme_request *req, void *arg),
 				      void *arg);
+
+	int (*qpair_get_fd)(struct spdk_nvme_qpair *qpair);
 
 	void (*admin_qpair_abort_aers)(struct spdk_nvme_qpair *qpair);
 

lib/nvme/nvme_ctrlr.c
@@ -422,6 +422,20 @@ spdk_nvme_ctrlr_disconnect_io_qpair(struct spdk_nvme_qpair *qpair)
 	nvme_ctrlr_unlock(ctrlr);
 }
 
+int
+spdk_nvme_ctrlr_qpair_get_fd(struct spdk_nvme_qpair *qpair)
+{
+	struct spdk_nvme_ctrlr *ctrlr = qpair->ctrlr;
+
+	return nvme_transport_qpair_get_fd(ctrlr, qpair);
+}
+
+int
+spdk_nvme_ctrlr_get_admin_qp_fd(struct spdk_nvme_ctrlr *ctrlr)
+{
+	return spdk_nvme_ctrlr_qpair_get_fd(ctrlr->adminq);
+}
+
 struct spdk_nvme_qpair *
 spdk_nvme_ctrlr_alloc_io_qpair(struct spdk_nvme_ctrlr *ctrlr,
 			       const struct spdk_nvme_io_qpair_opts *user_opts,