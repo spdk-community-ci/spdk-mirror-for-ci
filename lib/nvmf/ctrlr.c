spdk/spdk

commit 05632afd77dd6df3a7b46051232df30f15e424f7

nvmf: large IU and atomic write unit reporting
Added spdk_nvmf_ctrlr_identify_ns_ext() to request the identify
namespace data and populate performance and atomic operations fields.

Change-Id: Ib809b174e08a4b8a26bbc6d396ff3fb4798fae98
Signed-off-by: Atul Malakar <a.malakar@samsung.com>
Reviewed-on: https://review.spdk.io/gerrit/c/spdk/spdk/+/24094
Community-CI: Mellanox Build Bot
Reviewed-by: Konrad Sztyber <konrad.sztyber@intel.com>
Reviewed-by: Jim Harris <jim.harris@samsung.com>
Tested-by: SPDK CI Jenkins <sys_sgci@intel.com>

lib/nvmf/ctrlr.c
@@ -2759,11 +2759,11 @@ _nvmf_ctrlr_get_ns_safe(struct spdk_nvmf_ctrlr *ctrlr,
 	return ns;
 }
 
-int
-spdk_nvmf_ctrlr_identify_ns(struct spdk_nvmf_ctrlr *ctrlr,
-			    struct spdk_nvme_cmd *cmd,
-			    struct spdk_nvme_cpl *rsp,
-			    struct spdk_nvme_ns_data *nsdata)
+static void
+nvmf_ctrlr_identify_ns(struct spdk_nvmf_ctrlr *ctrlr,
+		       struct spdk_nvme_cmd *cmd,
+		       struct spdk_nvme_cpl *rsp,
+		       struct spdk_nvme_ns_data *nsdata)
 {
 	struct spdk_nvmf_subsystem *subsystem = ctrlr->subsys;
 	struct spdk_nvmf_ns *ns;
@@ -2772,7 +2772,7 @@ spdk_nvmf_ctrlr_identify_ns(struct spdk_nvmf_ctrlr *ctrlr,
 
 	ns = _nvmf_ctrlr_get_ns_safe(ctrlr, cmd->nsid, rsp);
 	if (ns == NULL) {
-		return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
+		return;
 	}
 
 	nvmf_bdev_ctrlr_identify_ns(ns, nsdata, ctrlr->dif_insert_or_strip);
@@ -2803,8 +2803,87 @@ spdk_nvmf_ctrlr_identify_ns(struct spdk_nvmf_ctrlr *ctrlr,
 			nsdata->nuse = 0;
 		}
 	}
+}
+
+int
+spdk_nvmf_ctrlr_identify_ns(struct spdk_nvmf_ctrlr *ctrlr,
+			    struct spdk_nvme_cmd *cmd,
+			    struct spdk_nvme_cpl *rsp,
+			    struct spdk_nvme_ns_data *nsdata)
+{
+	nvmf_ctrlr_identify_ns(ctrlr, cmd, rsp, nsdata);
 
 	return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
+}
+
+static void
+identify_ns_passthru_cb(struct spdk_nvmf_request *req)
+{
+	struct spdk_nvme_cmd *cmd = spdk_nvmf_request_get_cmd(req);
+	struct spdk_nvmf_ctrlr *ctrlr = spdk_nvmf_request_get_ctrlr(req);
+	struct spdk_nvme_cpl *rsp = spdk_nvmf_request_get_response(req);
+	struct spdk_nvme_ns_data nvmf_nsdata = {};
+	struct spdk_nvme_ns_data nvme_nsdata = {};
+	size_t datalen;
+
+	/* This is the identify data from the NVMe drive */
+	datalen = spdk_nvmf_request_copy_to_buf(req, &nvme_nsdata,
+						sizeof(nvme_nsdata));
+
+	nvmf_ctrlr_identify_ns(ctrlr, cmd, rsp, &nvmf_nsdata);
+
+	/* Update fabric's namespace according to SSD's namespace */
+	if (nvme_nsdata.nsfeat.optperf) {
+		nvmf_nsdata.nsfeat.optperf = nvme_nsdata.nsfeat.optperf;
+		nvmf_nsdata.npwg = nvme_nsdata.npwg;
+		nvmf_nsdata.npwa = nvme_nsdata.npwa;
+		nvmf_nsdata.npdg = nvme_nsdata.npdg;
+		nvmf_nsdata.npda = nvme_nsdata.npda;
+		nvmf_nsdata.nows = nvme_nsdata.nows;
+	}
+
+	if (nvme_nsdata.nsfeat.ns_atomic_write_unit) {
+		nvmf_nsdata.nsfeat.ns_atomic_write_unit = nvme_nsdata.nsfeat.ns_atomic_write_unit;
+		nvmf_nsdata.nawun = nvme_nsdata.nawun;
+		nvmf_nsdata.nawupf = nvme_nsdata.nawupf;
+		nvmf_nsdata.nacwu = nvme_nsdata.nacwu;
+	}
+
+	nvmf_nsdata.nabsn = nvme_nsdata.nabsn;
+	nvmf_nsdata.nabo = nvme_nsdata.nabo;
+	nvmf_nsdata.nabspf = nvme_nsdata.nabspf;
+
+	spdk_nvmf_request_copy_from_buf(req, &nvmf_nsdata, datalen);
+}
+
+int
+spdk_nvmf_ctrlr_identify_ns_ext(struct spdk_nvmf_request *req)
+{
+	struct spdk_nvme_cmd *cmd = spdk_nvmf_request_get_cmd(req);
+	struct spdk_nvmf_ctrlr *ctrlr = spdk_nvmf_request_get_ctrlr(req);
+	struct spdk_nvme_cpl *rsp = spdk_nvmf_request_get_response(req);
+	struct spdk_bdev *bdev;
+	struct spdk_bdev_desc *desc;
+	struct spdk_io_channel *ch;
+	struct spdk_nvme_ns_data nsdata = {};
+	struct spdk_iov_xfer ix;
+	int rc;
+
+	nvmf_ctrlr_identify_ns(ctrlr, cmd, rsp, &nsdata);
+
+	rc = spdk_nvmf_request_get_bdev(cmd->nsid, req, &bdev, &desc, &ch);
+	if (rc) {
+		return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
+	}
+
+	if (!spdk_bdev_io_type_supported(bdev, SPDK_BDEV_IO_TYPE_NVME_ADMIN)) {
+		spdk_iov_xfer_init(&ix, req->iov, req->iovcnt);
+		spdk_iov_xfer_from_buf(&ix, &nsdata, sizeof(nsdata));
+
+		return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
+	}
+
+	return spdk_nvmf_bdev_ctrlr_nvme_passthru_admin(bdev, desc, ch, req, identify_ns_passthru_cb);
 }
 
 static void
@@ -3287,8 +3366,9 @@ nvmf_ctrlr_identify(struct spdk_nvmf_request *req)
 
 	switch (cns) {
 	case SPDK_NVME_IDENTIFY_NS:
-		ret = spdk_nvmf_ctrlr_identify_ns(ctrlr, cmd, rsp, (void *)&tmpbuf);
-		break;
+		/* Function below can be asynchronous & we always need to have the data in request's buffer
+		 * So just return here */
+		return spdk_nvmf_ctrlr_identify_ns_ext(req);
 	case SPDK_NVME_IDENTIFY_CTRLR:
 		ret = spdk_nvmf_ctrlr_identify_ctrlr(ctrlr, (void *)&tmpbuf);
 		break;

test/unit/lib/nvmf/ctrlr.c/ctrlr_ut.c
@@ -225,6 +225,9 @@ DEFINE_STUB(nvmf_auth_request_exec, int, (struct spdk_nvmf_request *r),
 DEFINE_STUB(spdk_nvmf_subsystem_get_nqn, const char *,
 	    (const struct spdk_nvmf_subsystem *subsystem), NULL);
 
+DEFINE_STUB(spdk_bdev_io_type_supported, bool,
+	    (struct spdk_bdev *bdev, enum spdk_bdev_io_type io_type), false);
+
 void
 nvmf_qpair_set_state(struct spdk_nvmf_qpair *qpair, enum spdk_nvmf_qpair_state state)
 {

test/unit/lib/nvmf/tcp.c/tcp_ut.c
@@ -266,7 +266,8 @@ DEFINE_STUB(nvmf_qpair_auth_init, int, (struct spdk_nvmf_qpair *q), 0);
 DEFINE_STUB(nvmf_auth_request_exec, int, (struct spdk_nvmf_request *r),
 	    SPDK_NVMF_REQUEST_EXEC_STATUS_ASYNCHRONOUS);
 DEFINE_STUB(nvmf_request_get_buffers_abort, bool, (struct spdk_nvmf_request *r), false);
-
+DEFINE_STUB(spdk_bdev_io_type_supported, bool,
+	    (struct spdk_bdev *bdev, enum spdk_bdev_io_type io_type), false);
 struct spdk_io_channel *
 spdk_accel_get_io_channel(void)
 {

include/spdk/nvmf_cmd.h
@@ -80,6 +80,19 @@ int spdk_nvmf_ctrlr_identify_ns(struct spdk_nvmf_ctrlr *ctrlr,
 				struct spdk_nvme_ns_data *nsdata);
 
 /**
+ * Fills the identify namespace attributes for the specified controller.
+ *
+ * This funtion uses nvme passthru for the namespaces that are backed by bdevs
+ * that support NVME_ADMIN IO type. It differs from spdk_nvmf_ctrlr_identify_ns
+ * by requesting identify namespace data and populate performance and atomic
+ * operations fields.
+ *
+ * \param req The NVMe-oF request
+ * \return \ref spdk_nvmf_request_exec_status
+ */
+int spdk_nvmf_ctrlr_identify_ns_ext(struct spdk_nvmf_request *req);
+
+/**
  * Callback function definition for a custom admin command handler.
  *
  * A function of this type is passed to \ref spdk_nvmf_set_custom_admin_cmd_hdlr.

lib/nvmf/spdk_nvmf.map
@@ -102,6 +102,7 @@
 	spdk_nvmf_ctrlr_identify_ctrlr;
 	spdk_nvmf_ctrlr_identify_iocs_specific;
 	spdk_nvmf_ctrlr_identify_ns;
+	spdk_nvmf_ctrlr_identify_ns_ext;
 	spdk_nvmf_set_custom_admin_cmd_hdlr;
 	spdk_nvmf_set_passthru_admin_cmd;
 	spdk_nvmf_bdev_ctrlr_nvme_passthru_admin;

spdk-community-ci/spdk-mirror-for-ci

commit 902ed5681e067fc79fb948e04663351ea812556a

nvmf: use bdev's nsid for admin command passthru
The ID of a namespace exposed by an NVMe-oF subsystem can differ from
the nsid of the underlying namespace that backs a given bdev.  For
instance, the following:

  $ scripts/rpc.py bdev_nvme_attach_controller -b nvme0 ...
  nvme0n1
  nvme0n2
  $ scripts/rpc.py nvmf_subsystem_add_ns -n 2 nqn nvme0n1
  $ scripts/rpc.py nvmf_subsystem_add_ns -n 1 nqn nvme0n2

results in nvme0n1 being exposed with nsid=2, while nvme0n2 with nsid=1.
In this scenario, admin commands targeting a namespace (e.g. identify
namespace) would get sent to the wrong namespace.

Fixes: 05632afd77dd ("nvmf: large IU and atomic write unit reporting")
Reported-by: Maciej Szulik <maciej.szulik@intel.com>
Signed-off-by: Konrad Sztyber <konrad.sztyber@intel.com>
Change-Id: Ib4f7071afb10691c88def880fadc5f4d7e134b07

lib/nvmf/ctrlr.c
@@ -2892,6 +2892,7 @@ spdk_nvmf_ctrlr_identify_ns_ext(struct spdk_nvmf_request *req)
 		return SPDK_NVMF_REQUEST_EXEC_STATUS_COMPLETE;
 	}
 
+	cmd->nsid = spdk_bdev_get_nsid(bdev);
 	return spdk_nvmf_bdev_ctrlr_nvme_passthru_admin(bdev, desc, ch, req, identify_ns_passthru_cb);
 }
 
@@ -5008,6 +5009,8 @@ nvmf_passthru_admin_cmd_for_bdev_nsid(struct spdk_nvmf_request *req, uint32_t bd
 
 	if (ns->passthrough_nsid) {
 		req->cmd->nvme_cmd.nsid = ns->passthrough_nsid;
+	} else {
+		req->cmd->nvme_cmd.nsid = spdk_bdev_get_nsid(bdev);
 	}
 
 	return spdk_nvmf_bdev_ctrlr_nvme_passthru_admin(bdev, desc, ch, req, NULL);

test/unit/lib/nvmf/ctrlr.c/ctrlr_ut.c
@@ -227,6 +227,7 @@ DEFINE_STUB(spdk_nvmf_subsystem_get_nqn, const char *,
 
 DEFINE_STUB(spdk_bdev_io_type_supported, bool,
 	    (struct spdk_bdev *bdev, enum spdk_bdev_io_type io_type), false);
+DEFINE_STUB(spdk_bdev_get_nsid, uint32_t, (struct spdk_bdev *b), 0);
 
 void
 nvmf_qpair_set_state(struct spdk_nvmf_qpair *qpair, enum spdk_nvmf_qpair_state state)

test/unit/lib/nvmf/tcp.c/tcp_ut.c
@@ -267,6 +267,8 @@ DEFINE_STUB(nvmf_auth_request_exec, int, (struct spdk_nvmf_request *r),
 DEFINE_STUB(nvmf_request_get_buffers_abort, bool, (struct spdk_nvmf_request *r), false);
 DEFINE_STUB(spdk_bdev_io_type_supported, bool,
 	    (struct spdk_bdev *bdev, enum spdk_bdev_io_type io_type), false);
+DEFINE_STUB(spdk_bdev_get_nsid, uint32_t, (struct spdk_bdev *b), 0);
+
 struct spdk_io_channel *
 spdk_accel_get_io_channel(void)
 {