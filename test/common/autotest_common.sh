f56ce2b18c81534a35fc594e474a80b5b9cca9af
Michal Berger <michal.berger@intel.com>
2024-10-28 09:37:58 +0100

    test/common: [TEST] Simplify all_tests search
    Change-Id: If738026a0b8a6f358cec4477de1555673b44980a
    Signed-off-by: Michal Berger <michal.berger@intel.com>

--- test/common/autotest_common.sh
+++ test/common/autotest_common.sh
@@ -750,11 +750,8 @@ function timing_finish() {
 
 function create_test_list() {
 	xtrace_disable
-	# First search all scripts in main SPDK directory.
-	completion=$(grep -shI -d skip --include="*.sh" -e "run_test " $rootdir/*)
-	# Follow up with search in test directory recursively.
-	completion+=$'\n'$(grep -rshI --include="*.sh" --exclude="*autotest_common.sh" -e "run_test " $rootdir/test)
-	printf "%s" "$completion" | grep -v "#" \
+	git grep -Ih "run_test " $(git ls-files '*.sh' ':!:*autotest_common.sh') \
+		| grep -v "#" \
 		| sed 's/^.*run_test/run_test/' | awk '{print $2}' \
 		| sed 's/\"//g' | sort > $output_dir/all_tests.txt || true
 	xtrace_restore
